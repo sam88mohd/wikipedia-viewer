7690afe1cc97ec39339090f9c48a20c5076910cb
sam88mohd <sam88mohd@gmail.com>
2021-12-30 12:46:11 +0800

    styling app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+  },
+});
+
 const App = () => {
-  return <div>Hello</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Typography variant="h4">Wikipedia Viewer</Typography>
+    </div>
+  );
 };
 
 export default App;
